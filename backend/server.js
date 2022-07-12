f0ec9a8495b3c7b1c245a22fc47043fcb64362b1
MateuszSzczukowski <mateusz.szczukowski@gmail.com>
2022-07-12 17:00:26 +0200

    Refactoring and clean up code

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const dataTrips = require("./data/trips.json");
+const fs = require("fs");
 const cors = require("cors");
 
 app.use(cors());
@@ -9,6 +9,12 @@ app.listen(3005, () => {
     console.log("Server is listening on port 3005");
 })
 
-app.get("/api/data", (req, res) => {
-    res.json(dataTrips);
+app.get('/api/data', (req, res) => {
+    fs.readFile('./data/trips.json', 'utf8', (err, data) => {
+        if(err) {
+            throw err;
+        }
+
+        res.send(JSON.parse(data));
+    })
 })
